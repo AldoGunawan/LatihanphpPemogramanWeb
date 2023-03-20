f11adf37f4b79f353eaa924f13dd7c56b242d2c5
ALDO GUNAWAN <107549731+AldoGunawan@users.noreply.github.com>
2023-03-20 11:20:36 +0700

    update title and value

--- login.php
+++ login.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>LatihanPhp</title>
 </head>
 <body>
     <h3>Login</h3><br>
     <form action="proses.php" method="get">
-    Username : <input type="text" name="uname" placeholder="Isi Username an"> <br>
-    Password : <input type="password" name="pwd" placeholder="Isi Pass ngen"> <br>
+    Username : <input type="text" name="uname" placeholder="Isi Username"> <br>
+    Password : <input type="password" name="pwd" placeholder="Isi Password"> <br>
     <input type="submit" value="Login">
 </form>
 </body>
